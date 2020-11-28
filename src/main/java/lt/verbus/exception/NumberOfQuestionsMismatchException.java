c34b87c26b36da074be1100dbbbf7394f505952a
Sarunas Verbus <sarunas@verbus.lt>
2020-11-28 13:05:10 +0200

    remove empty constructor

--- src/main/java/lt/verbus/exception/NumberOfQuestionsMismatchException.java
+++ src/main/java/lt/verbus/exception/NumberOfQuestionsMismatchException.java
@@ -2,9 +2,6 @@ package lt.verbus.exception;
 
 public class NumberOfQuestionsMismatchException extends Exception {
 
-    public NumberOfQuestionsMismatchException() {
-    }
-
     public NumberOfQuestionsMismatchException(String message) {
         super(message);
     }
