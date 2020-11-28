450097c383ad7b6fd5366f8cf250fb5e834026f4
Sarunas Verbus <sarunas@verbus.lt>
2020-11-28 12:58:59 +0200

    change hibernate ID generation type to IDENTITY

--- src/main/java/lt/verbus/domain/entity/Answer.java
+++ src/main/java/lt/verbus/domain/entity/Answer.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 public class Answer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
--- src/main/java/lt/verbus/domain/entity/User.java
+++ src/main/java/lt/verbus/domain/entity/User.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
